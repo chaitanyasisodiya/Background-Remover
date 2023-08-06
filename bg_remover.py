4c0199f71d458463ee276189b4d7ba971f8b4aa7
Chaitanya  Jitendrasing  Girase <45315166+chaitanyasisodiya@users.noreply.github.com>
2023-08-06 23:24:21 +0530

    update bg

--- bg_remover.py
+++ bg_remover.py
@@ -9,6 +9,7 @@ st.set_page_config(layout="wide", page_title="Image Background Remover")
 st.write("## Remove background from image using live camera or upload image ")
 st.sidebar.write("## Upload and download :gear:")
 
+
 # Download the fixed image
 def convert_image(img):
     buf = BytesIO()
@@ -25,14 +26,14 @@ def fix_image(upload):
     col2.write("Fixed Image :wrench:")
     col2.image(fixed)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image",
-                convert_image(fixed), "fixed.png", "image/png")
+    st.sidebar.download_button("Download fixed image",convert_image(fixed), "fixed.png", "image/png")
+
 
 col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
 pic = st.camera_input('Take a picture')
 if pic:
-    with open ('test.jpg','wb') as file:
+    with open('test.jpg', 'wb') as file:
         file.write(pic.getbuffer())
 
 if my_upload is not None:
